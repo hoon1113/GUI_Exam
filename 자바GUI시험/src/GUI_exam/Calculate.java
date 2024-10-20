eec8d6a7c6d672899a18f98b63129bc20803a8c0
hoon1113 <82316417+hoon1113@users.noreply.github.com>
2024-10-20 22:05:55 +0900

    디자인 코드 간결화 및 이벤트 넣기 시작
    버튼 클릭 했을 때 텍스트에 적히는 것까지 구현

--- 자바GUI시험/src/GUI_exam/Calculate.java
+++ 자바GUI시험/src/GUI_exam/Calculate.java
@@ -15,66 +15,65 @@ import javax.swing.JTextField;
  * @version 0.0.2
  */
 public class Calculate extends JFrame {
-	JPanel p1; // 상단 패널
-	JPanel p2; // 하단 패널
-	String[] btnText1 = { "(", ")", "%", "CE", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ",", "=", "+"};
+	JPanel p2;
+	String[] btnText1 = { "(", ")", "%", "CE", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"};
 	/**
 	 * @param String[] btnText1
 	 * <br>
 	 * 버튼 안에 들어갈 문자를 String 배열을 이용해 만들었음
 	 */
-	JButton[] b1 = new JButton[20];
-	/**
-	 * @param JButton[] b1
-	 * <br>
-	 * 버튼 리스트를 생성해서 총 20개의 칸을 생성해준다.
-	 */
+	JButton[] b1;
 	JTextField t1; // 상단에 들어갈 텍스트 칸 생성
+	double result = 0; // 계산 결과
+	String operator; // 연산자
 
 	Calculate() {
 		this.setTitle("계산기");
 		this.setSize(260, 300);
 		this.setLayout(new BorderLayout());
-		p1 = new JPanel();
-		p1.setLayout(new BorderLayout(5, 5));
+		
+		t1 = new JTextField();
+		getContentPane().add(t1, BorderLayout.NORTH);
+		t1.setEnabled(false);
+		
 		p2 = new JPanel();
+		getContentPane().add(p2, BorderLayout.CENTER);
 		p2.setLayout(new GridLayout(5, 3, 2, 2));
 		/**
 		 * GridLayout안에 가로 3줄 새로 5줄 칸 형성, 칸 사이 간격 2, 2
-		 */
-		t1 = new JTextField();
-		t1.setEnabled(false);
-		this.add(p1, "North"); // 페널 북쪽에 위치
-		this.add(p2, "Center"); // 페널 중간에 위치
-		p1.add(t1, "West"); // p1안에서 서쪽에 위치
-		int a = 0; // for 문에서 버튼에 텍스트 값을 차례대로 넣기 위해 만든 변수
+		 */		
 		Color Light_Blue = new Color(30,144,255); // setBackground에 없는 색을 넣기 위함
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 4; j++) {
-				b1[j] = new JButton(btnText1[a]);
-				a += 1;
-				b1[j].setBackground(Color.DARK_GRAY);
-				b1[j].setForeground(Color.white);
-				if (a < 5 || a == 8 || a == 12 || a == 16 || a == 20) {
-					b1[j].setBackground(Color.GRAY);
-				}else if (a == 19) {
-					b1[j].setBackground(Light_Blue);
-					b1[j].setForeground(Color.BLACK);
-				}
-				/**
-				 * if 이용해 디자인 수정. <br>
-				 * 숫자 칸만 진한 회색, 다른 곳은 밝은 회색, = 값은 파랑으로 수정
-				 */
-				p2.add(b1[j]);
+		b1  = new JButton[20]; // 버튼 만들기 시작
+		for (int i = 0; i < b1.length; i++) {
+			// 버튼의 길이 만큼 반복해서 버튼 생성, 칸은 GridLayout이용해서 만들었음
+			b1[i] = new JButton("" + btnText1[i]);
+			b1[i].setBackground(Color.DARK_GRAY);
+			b1[i].setForeground(Color.white);
+			if (i < 4 || i == 7 || i == 11 || i == 15 || i == 19) {
+				b1[i].setBackground(Color.GRAY);
+			}else if(i == 18) {
+				b1[i].setBackground(Light_Blue);
+				b1[i].setForeground(Color.black);
 			}
+			/**
+			 *  if 문 이용해서 디자인함
+			 */
+			b1[i].addActionListener(e -> {
+				// 이벤트 넣기 시작
+				String cammand = e.getActionCommand();
+				if(cammand.equals("0") || cammand.equals("1") || cammand.equals("2") || cammand.equals("3") || cammand.equals("4") || cammand.equals("5") || cammand.equals("6") || cammand.equals("7") || cammand.equals("8") || cammand.equals("9")) {
+					t1.setText(t1.getText() + cammand);
+				} // 0~9 숫자 클릭시 숫자 텍스트에 입력
+				if(cammand.equals(".")) {
+					t1.setText(t1.getText() + ".");
+				} // 점 클릭시 텍스트에 점 입력
+				if(cammand.equals("CE")) {
+					t1.setText(t1.getText().substring(0, t1.getText().length() -1));
+					// 텍스트의 길이를 뒤에서부터 1개씩 줄이는 원리
+				}
+			});
+			p2.add(b1[i]);
 		}
-		/**
-		 * 2중 for 문 사용 <br>
-		 * 버튼생성은 왼쪽에서 오른쪽, 위에서 아래로 생성 <br>
-		 * 버튼의 색상은 회색, 텍스트 색상은 흰색 <br>
-		 * @see 버튼 중복 생성 블로그 참조 <br>
-		 * @link https://intunknown.tistory.com/entry/%EC%9E%90%EB%B0%94-GUI-%EB%B2%84%ED%8A%BC-%EB%B0%B0%EC%97%B4%EC%BD%94%EB%93%9C-%EC%A4%91%EB%B3%B5
-		 */
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
